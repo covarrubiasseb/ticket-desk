2069d92a8c5b4eb807c3b45cebf21fb9b7abd009
unknown <covarrubias.seb@gmail.com>
2023-01-21 14:22:13 -0800

    implement login/logout with Google 0auth

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
-let express = require('express')
-let app = express()
+const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+require('./passport')
+
+const app = express()
 const port = 3000
 
-app.use(express.static('www'))
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next()
+  } else {
+    res.redirect('/google')
+  }
+}
+
+app.use(session({
+  secret: 'google-auth-session',
+  keys: ['key1', 'key2']
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  if (req.user) {
+    res.redirect('/success')
+  } else {
+    res.redirect('/google')
+  }
+})
+
+app.get('/failed', (req, res) => {
+  res.send('<h1>Failed</h1>')
+})
+
+app.use('/success', isLoggedIn, express.static('www'))
+
+app.get('/google', 
+        passport.authenticate('google', { scope: ['email', 'profile'] })
+)
+
+app.get('/google/callback',
+        passport.authenticate('google', { failureRedirect: '/failed' }),
+        (req, res) => {
+          res.redirect('/success')
+        }
+)
+
+app.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) { return next(err) }
+    res.redirect('/')
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
---
+++ passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+passport.serializeUser( (user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser( (user, done) => {
+  done(null, user)
+})
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: '270622692344-0mkd6u3c89d281b3dg6gpolqea1b7ggn.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:3000/google/callback',
+    passReqToCallback: true
+  },
+
+  (request, accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
+  }
+))
