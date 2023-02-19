6d58578653509aa903989ea5d257a59ea56021d9
unknown <covarrubias.seb@gmail.com>
2023-02-18 17:47:47 -0800

    Fixed home page button (splash)

--- ticket-desk/src/Main.js
+++ ticket-desk/src/Main.js
@@ -17,7 +17,7 @@ class Main extends React.Component {
             <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
                 
-                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#" onClick={this.props.setPageDashboard}>
                     <div className="sidebar-brand-icon rotate-n-15">
                         <i className="fas fa-tasks"></i>
                     </div>
