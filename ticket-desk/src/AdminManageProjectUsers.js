e343fb00b53416f1c86151179b241cd3f29a3cd4
unknown <covarrubias.seb@gmail.com>
2023-02-21 22:30:07 -0800

    Display all users in Admin Manage Projects User page.

--- ticket-desk/src/AdminManageProjectUsers.js
+++ ticket-desk/src/AdminManageProjectUsers.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
 class AdminManageProjectUsers extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      users: []
+    }
+
+    this.getAllUsers = this.getAllUsers.bind(this);
+  }
+
+  getAllUsers() {
+    axios.get(`/api/users`, this.props.headersConfig)
+    .then(response => {
+      if (response.data.valid) {
+
+        let users = response.data.results;
+
+        this.setState({
+
+          users: users.map(user => {
+
+            return (
+            
+              <tr>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+              </tr>
+
+            );
+
+          })
+
+        });
+
+      } else {
+        console.log("Could Not Get Users");
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getAllUsers();
   }
 
   render() {
@@ -16,8 +59,26 @@ class AdminManageProjectUsers extends React.Component {
         </div>
 
         <div className="card-body">
+
+          <table className="table table-hover">
+
+            <thead className="table-light text-dark">
+              <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">email</th>
+                <th scope="col">Role</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.users}
+            </tbody>
+
+          </table>
+
         </div>
-        
+
       </div>
 
     );
