306621d3a3909b9bf6b6b59982ea6bebb306766d
unknown <covarrubias.seb@gmail.com>
2023-02-22 19:48:27 -0800

    hasUsers flag in test

--- test/Test.js
+++ test/Test.js
@@ -5,22 +5,32 @@ const db = require('./DB_Test');
 
 const saltRounds = 10;
 
+const hasUsers = true;
+
 // Insert Users
-fs.readFile('./test/data/MOCK_DATA_USERS.json', 'utf-8', (err, stringified) => {
-  if (err) { console.log(err); }
-  else {
-    let data = JSON.parse(stringified);
-    data.forEach(user => {
 
-      bcrypt.hash(user.password, saltRounds, (err, hash) => {
+if (!hasUsers) {
 
-        mysql.connection.query(db.queries.createUser(user.firstName, user.lastName, user.email, hash), (err, results) => {
-          console.log(`User Added: ${hash}`);
+  fs.readFile('./test/data/MOCK_DATA_USERS.json', 'utf-8', (err, stringified) => {
+    if (err) { console.log(err); }
+    else {
+      let data = JSON.parse(stringified);
+      data.forEach(user => {
+
+        bcrypt.hash(user.password, saltRounds, (err, hash) => {
+
+          mysql.connection.query(db.queries.createUser(user.firstName, user.lastName, user.email, hash), (err, results) => {
+            console.log(`User Added: ${hash}`);
+          });
+
         });
 
       });
 
-    });
+      hasUsers = true;
 
-  }
-});
+    }
+  });
+
+}
+
