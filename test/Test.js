d13a7bf2c8fe44ad17f3fe14ae6704d4f331ad3a
unknown <covarrubias.seb@gmail.com>
2023-03-06 19:33:52 -0800

    codefix for populating demo data.

--- test/Test.js
+++ test/Test.js
@@ -23,19 +23,39 @@ async function createUsers(users) {
 
 }
 
-if (process.env.DB_TEST_HAS_USERS === 'false') {
+function PopulateData() {
 
-  fs.readFile('./test/data/MOCK_DATA_USERS.json', 'utf-8', (err, stringified) => {
-    if (err) { console.log(err); }
-    else {
-      let data = JSON.parse(stringified);
-      
-      createUsers(data);
+  if (process.env.DB_TEST_HAS_USERS === 'false') {
 
-    }
-  });
+    fs.readFile('./test/data/MOCK_DATA_USERS.json', 'utf-8', (err, stringified) => {
+      if (err) { console.log(err); }
+      else {
+        let data = JSON.parse(stringified);
+        
+        createUsers(data);
+
+      }
+    });
+
+  }
+
+  if (process.env.DB_TEST_HAS_PROJECTS === 'false') {
+    
+    fs.readFile('./test/data/MOCK_DATA_PROJECTS.json', 'utf-8', (err, stringified) => {
+      if (err) { console.log(err); }
+      else {
+        let data = JSON.parse(stringified);
+
+        createProjects(data);
+
+      }
+    });
+
+  }
 
 }
+
+
 
 // Insert Projects
 async function createProjects(projects) {
@@ -55,47 +75,4 @@ async function createProjects(projects) {
 
 }
 
-if (process.env.DB_TEST_HAS_PROJECTS === 'false') {
-  
-  fs.readFile('./test/data/MOCK_DATA_PROJECTS.json', 'utf-8', (err, stringified) => {
-    if (err) { console.log(err); }
-    else {
-      let data = JSON.parse(stringified);
-
-      createProjects(data);
-
-    }
-  });
-
-}
-
-async function setProjectsAdmin(projects) {
-
-  let index = 1;
-
-  for (const project of projects) {
-
-    await mysql.connection.promise().query(db.queries.addUserToProject(1, index))
-          .then( () => console.log(`userID: 1 Added to projectID: ${index}`));
-
-    index++;
-
-  }
-
-}
-
-// Set UserID 1 as Admin of Project
-
-if (process.env.DB_TEST_HAS_PROJECTS_ADMIN === 'false') {
-
-  fs.readFile('./test/data/MOCK_DATA_PROJECTS.json', 'utf-8', (err, stringified) => {
-    if (err) { console.log(err); }
-    else {
-      let data = JSON.parse(stringified);
-
-      setProjectsAdmin(data);
-
-    }
-  });
-
-}
+module.exports = PopulateData;
