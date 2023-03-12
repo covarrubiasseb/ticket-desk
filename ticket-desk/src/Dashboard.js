95dab7b99c4fc773187bfae9505d83ab0aa5b0b7
unknown <covarrubias.seb@gmail.com>
2023-03-12 04:28:23 -0700

    Created Display cards for Projects(total) and Tickets Created, with Tickets By Type Bar Chart.

--- ticket-desk/src/Dashboard.js
+++ ticket-desk/src/Dashboard.js
@@ -7,11 +7,15 @@ class Dashboard extends React.Component {
 
     this.state = {
       projectTotal: 0,
-      ticketTotal: 0
+      tickets: [],
+      ticketsTypeClientTotal: 0,
+      ticketsTypeServerTotal: 0,
+      ticketsTypeDevOpsTotal: 0
     }
 
     this.getProjectTotal = this.getProjectTotal.bind(this);
-    this.getTicketTotal = this.getTicketTotal.bind(this);
+    this.getTickets = this.getTickets.bind(this);
+    this.calculateTicketTypeTotals = this.calculateTicketTypeTotals.bind(this);
   }
 
   getProjectTotal() {
@@ -27,13 +31,17 @@ class Dashboard extends React.Component {
     });
   }
 
-  getTicketTotal() {
+  getTickets() {
     axios.get(`/api/user/tickets?userID=${this.props.userID}`, this.props.headersConfig)
     .then(response => {
 
       this.setState({
 
-        ticketTotal: response.data.length
+        tickets: response.data
+
+      }, () => {
+
+        this.calculateTicketTypeTotals();
 
       });
 
@@ -41,9 +49,38 @@ class Dashboard extends React.Component {
 
   }
 
+  calculateTicketTypeTotals() {
+    let clientTotal = 0;
+    let serverTotal = 0;
+    let devOpsTotal = 0;
+
+    this.state.tickets.forEach(ticket => {
+      if (ticket.type === 'Client Side') {
+        clientTotal++;
+      }
+
+      if (ticket.type === 'Server Side') {
+        serverTotal++;
+      }
+
+      if (ticket.type === 'Dev Ops') {
+        devOpsTotal++;
+      }
+    });
+
+    this.setState({
+
+      ticketsTypeClientTotal: clientTotal,
+      ticketsTypeServerTotal: serverTotal,
+      ticketsTypeDevOpsTotal: devOpsTotal 
+
+    });
+
+  }
+
   componentDidMount() {
     this.getProjectTotal();
-    this.getTicketTotal();
+    this.getTickets();
   }
 
   render() {
@@ -83,7 +120,7 @@ class Dashboard extends React.Component {
                         <div className="col mr-2">
                             <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Tickets Created</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.ticketTotal}</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.tickets.length}</div>
                         </div>
                         <div className="col-auto">
                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -96,6 +133,49 @@ class Dashboard extends React.Component {
 
         </div>
 
+        <div className="row">
+
+          <div className="col-12">
+
+            <div className="card shadow my-4">
+
+              <div className="card-header">
+                <h6 className="m-0 font-weight-bold text-primary">Tickets By Type</h6>
+              </div>
+
+              <div className="card-body">
+                
+                <div className="m-2">Client Side</div>
+                <div className="progress m-2">
+                  <div className="progress-bar bg-success" role="progressbar" style={{ width: `${Math.round( (this.state.ticketsTypeClientTotal / this.state.tickets.length) * 100 )}%`}} 
+                       aria-valuenow={Math.round(this.state.ticketsTypeClientTotal / this.state.tickets.length)} aria-valuemin="0" aria-valuemax={this.state.tickets.length}>
+                    <span className="text-gray-800 font-weight-bold">{this.state.ticketsTypeClientTotal}</span>
+                  </div>
+                </div>
+
+                <div className="m-2">Server Side</div>
+                <div className="progress m-2">
+                  <div className="progress-bar bg-info" role="progressbar" style={{ width: `${Math.round( (this.state.ticketsTypeServerTotal / this.state.tickets.length) * 100 )}%`}} 
+                       aria-valuenow={Math.round(this.state.ticketsTypeServerTotal / this.state.tickets.length)} aria-valuemin="0" aria-valuemax={this.state.tickets.length}>
+                    <span className="text-gray-800 font-weight-bold">{this.state.ticketsTypeServerTotal}</span>
+                  </div>
+                </div>
+
+                <div className="m-2">Dev Ops</div>
+                <div className="progress m-2">
+                  <div className="progress-bar bg-warning" role="progressbar" style={{ width: `${Math.round( (this.state.ticketsTypeDevOpsTotal / this.state.tickets.length) * 100 )}%`}} 
+                       aria-valuenow={Math.round(this.state.ticketsTypeDevOpsTotal / this.state.tickets.length)} aria-valuemin="0" aria-valuemax={this.state.tickets.length}>
+                    <span className="text-gray-800 font-weight-bold">{this.state.ticketsTypeDevOpsTotal}</span>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
 
       </div>
     );
